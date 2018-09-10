5d1f63295167f0440733a01bb09ea57f361d9447
Erkki Sula <erkkisula@users.noreply.github.com>
2018-09-10 10:40:53 +0300

    html muudetud php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Erkki Sula lehekülg</title>
+</head>
+<body>
+	<h1>Erkki Sula lehekülg</h1>
+	<p>Siin on minu <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames valminud veebilehed. See ei oma mingit sisu ja nende kopeerimine ei oma mõtet.</p>
+	<p>Kes elab metsa sees? 
+	Kes elab paksu metsa sees? 
+	Kes elab metsa sees? </p>
+	<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+	<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">
+	<p>Mul on sõber, kes ka teeb <a href="../../~maksjel" target="_blank">veebi</a>.</p>
+</body>
+</html>
